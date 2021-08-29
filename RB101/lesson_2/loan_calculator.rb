bdbec98dae56be9e32242c867759207627620ab3
christina <chrstnaheadley@gmail.com>
2021-08-29 19:38:57 -0400

    starts loan calc lesson 2

--- RB101/lesson_2/loan_calculator.rb
+++ RB101/lesson_2/loan_calculator.rb
@@ -24,9 +24,36 @@
 
 require 'yaml'
 MESSAGES = YAML.load_file('loan_messages.yml')
-puts MESSAGES.inspect
+
 # apr
 # loan
 # length
 # monthly_payment
 # monthly_rate
+def duration_valid?(loan_length)
+  /\d/.match(loan_length)
+end
+def apr_valid?(apr)
+  apr >=0 && apr <= 20
+end
+def loan_amount_valid?(loan_amount)
+  loan_amount > 0 && loan_amount < 500_000
+end
+
+
+puts(MESSAGES['intro'])
+loan_amount = 0
+until loan_amount_valid?(loan_amount)
+  puts(MESSAGES['loan_text'])
+  loan_amount = gets.to_i
+
+end
+puts(MESSAGES['length_text'])
+loan_length = gets.to_i
+puts(MESSAGES['apr_text'])
+apr = (gets.to_f) * 0.01
+monthly_rate = apr / 12
+monthly_payment = loan_amount * (monthly_rate/ (1 - (1 + monthly_rate)**(-loan_length)))
+puts apr
+puts monthly_rate
+puts monthly_payment
